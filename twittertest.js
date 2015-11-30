42a6814ab4f215c5cefb425dc65100ca5df5dbf4
1205982 <1205982@rgu.ac.uk>
2015-11-30 14:20:01 +0000

    node-commit-instruction

--- twittertest.js
+++ twittertest.js
@@ -2,12 +2,22 @@
  * Created by 1205982 on 30/11/2015.
  */
 var Twitter = require('twitter');
+var http = require('http')
+var port = process.env.PORT || 1337;
+
 var client = new Twitter({
     consumer_key: 'xdB6ezpAKZxWXzJlMN4vSgkZy',
     consumer_secret: 'F9gfZ3tSZYdHat0ApRrEq8avSs4hRTOzg4q6gi0WROKYaM8rar',
     access_token_key: '2273543417-FkYr8myKzYfQvkoB6GSeTt9EFr8QuNYcKfIDTUy',
     access_token_secret: '6CerFW59Fyo9w9uHRHDjybVcuVe6ENm2AbJlItWFikLuX'
 });
-client.get('search/tweets', {q: 'lolcat', count:'2'}, function(error, tweets){
-    console.log(tweets);
-});
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
+        console.log(tweets);
+    });
+
+}).listen(port);
+
